4e0b9497488b4fd892e417caa862c3d53f5e57f4
MacarenaBenguigui <130480569+MacarenaBenguigui@users.noreply.github.com>
2024-03-12 11:20:15 +0100

    html nuevo

---
+++ html_code.py
@@ -0,0 +1,20 @@
+
+icon_url = "https://i.postimg.cc/mDCmmtp8/Logo2-sinfondo-x1024.png"
+
+# HTML para el título e ícono
+title_html = f"""
+<div style="font-size: 35px; font-weight: bold; display: flex; align-items: center;">
+    <img src="{icon_url}" style="width: 48px; margin-right: 15px;">
+    Inneva Chat
+</div>
+"""
+
+def generate_html_with_icon(icon_url, text, icon_size="100px", text_margin="20px", space_between="30px", text_size="20px"):
+    """Genera una cadena HTML para mostrar un ícono y texto con tamaño, margen, espacio entre conjuntos personalizados y tamaño de texto."""
+    html_string = f'''
+    <div style="margin-bottom: {space_between}; display: flex; align-items: center;">
+        <img src="{icon_url}" style="width: {icon_size}; display: inline-block; margin-right: {text_margin};">
+        <span style="font-size: {text_size};">{text}</span>
+    </div>
+    '''
+    return html_string
