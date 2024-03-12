f24af3c5f95f88f02ae676f40f0487a832e01a95
MacarenaBenguigui <130480569+MacarenaBenguigui@users.noreply.github.com>
2024-03-12 15:53:09 +0100

    Update html_code.py

--- html_code.py
+++ html_code.py
@@ -14,14 +14,38 @@ html_hide = f"""
 <link rel="shortcut icon" type="image/png" href="{icon_url}">
 </head>
 <style>
-
     [data-testid="stToolbar"] {{visibility: hidden !important;}}
-
     MainMenu {{visibility: hidden;}}
-
     footer {{visibility: hidden !important;}}
-  
     header {{visibility: hidden;}} 
+
+    /* Aquí añades el nuevo estilo para ocultar la barra inferior, si fuera necesario */
+    .wrapper {{
+        position: relative;
+        z-index: 1;
+        display: inline-block;
+        width: 100vw;
+    }}
+
+    .hidefooter {{
+        position: absolute;
+        width: 150px;
+        height: 35px;
+        background: rgb(242,240,246);
+        right: 0px;
+        bottom: 0px;
+        z-index: 2;
+        display: block;
+        color: rgb(0, 0, 0);
+    }}
+
+    iframe {{
+        display: block;
+        background: #ffffff;
+        border: none;
+        height: 99vh;
+        width: 99vw;
+    }}
 </style>
 """
 
